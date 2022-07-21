e85a2d10db0774d64116f6a93f1165da8c4a3108
faisal-kn <khanmohdfaisal75@gmail.com>
2022-07-21 20:11:47 +0530

    done

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Comments from "./Comments";
+import { useState, useEffect } from "react";
+
+import data from "../data/db.json";
+import "../styles/App.css";
+
+function App() {
+  return (
+
+        <main className="App">
+          
+        </main>
+  
+  );
+}
+
+export default App;
